a8751cad81bef3f0cb1589412748ea0c34df05db
mananthacker <thackermanan1112@gmail.com>
2025-01-17 12:16:16 +0530

    Add files via upload

---
+++ manan voting age.py
@@ -0,0 +1,8 @@
+def major_minor():
+    age = int(input("Enter your age:"))
+    if age < 18:
+        print("You are still Minor.")
+    else:
+        print("You have become Major, now.")
+
+major_minor()
